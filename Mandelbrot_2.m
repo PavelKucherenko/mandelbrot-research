de04748bdd0c560ffd0f7a334b2bec74fb57d13e
PavelKucherenko <47821537+PavelKucherenko@users.noreply.github.com>
2021-04-29 00:54:28 +0300

    3d Mandelbrot bifurcation plot is added

--- Mandelbrot_2.m
+++ Mandelbrot_2.m
@@ -3,9 +3,9 @@ c_real_min = -2;    c_real_max = 0.6;
 c_imag_min = -1.3;  c_imag_max = 1.3;
 
 % количество точек (координат) построения 
-n_crd = 10;
+n_crd = 200;
 % количество итераций
-n_iter = 50;
+n_iter = 100;
 % радиус убегания
 r_escape = 2;
 
@@ -21,13 +21,18 @@ for iter_idx = 1:n_iter
     k(abs(z) > r_escape & k == 0) = n_iter - iter_idx;
 end
 
-figure,
-imagesc(k),
+figure()
+movegui('west')
+
+imagesc(k)
 colormap hot
 axis square 
 
 z_plot = abs(z);
 z_plot(z_plot>r_escape)=NaN;
+
+figure()
+movegui('north')
 mesh(c_real, c_imag, z_plot)
 
 % Построение множества Мандельброта с бифуркацией в 3d
@@ -37,5 +42,16 @@ for iter_idx = 2:n_iter
     z_vals(:,:,iter_idx) = z_vals(:,:,iter_idx-1).^2 + c;
 end
 
-z_plot_vals = abs(z_vals);
-z_plot_vals(z_plot_vals>r_escape)=NaN;
+z_vals_plot = abs(z_vals);
+z_vals_plot(z_vals_plot > r_escape) = NaN;
+
+n_plots_to_mix = 50;
+
+figure()
+movegui('east')
+
+for plot_mix_idx = 0:n_plots_to_mix-1
+    mesh(c_real, c_imag, z_vals_plot(:,:,end-plot_mix_idx))
+    hold on
+end
+hold off
